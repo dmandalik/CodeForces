2f801bc91ea0c3c9c4583b75d2afdbfc7d463ecf
Dhruv M <dhruvm2310@gmail.com>
2026-06-11 15:07:04 -0400

    graycode

--- CSES/GrayCode.cpp
+++ CSES/GrayCode.cpp
@@ -7,11 +7,20 @@ using namespace std;
 
 
 int main() {
-    long long n;
-    cin >> n;
-    
-    n = n || (n - 1);
-    cout << n;
-    
+    int n;
 
+    cin >> n;
+
+    int total = 1 << n; // 2^n
+    
+    for (int i = 0; i < total; i++) {
+        int gray = i ^ (i >> 1);
+        for (int bit = n - 1; bit >= 0; bit--) {
+            cout << ((gray >> bit) & 1);
+        }
+        cout << "\n";
+    }
+
+    return 0;
+    
 }
